cd4b3477db29a75841c42ff97c4dba42a8327729
connorgit32 <55241602+connorgit32@users.noreply.github.com>
2021-03-20 20:53:10 -0400

    Add hero working

--- nodegui-starter/src/index.js
+++ nodegui-starter/src/index.js
@@ -1,32 +1,34 @@
-import { QMainWindow, QWidget, QLabel, FlexLayout, QPushButton, QIcon, QSize, QPlainTextEdit, QMenu, QTextEdit   } from '@nodegui/nodegui';
+import { QMainWindow, QWidget, QLabel, FlexLayout, QPushButton, QIcon, QSize, QPlainTextEdit, QLineEdit, QMenu, QTextEdit   } from '@nodegui/nodegui';
 import logo from '../assets/logox200.png';
+
 const mysql = require('mysql');
 const database = mysql.createConnection({
-    host    :'localhost',
-    user    :'nodejs',
-    password:'nodejspassword',
-    database:'superheroes'
+  host    :'localhost',
+  user    :'nodejs',
+  password:'nodejspassword',
+  database:'superheroes'
 });
 database.connect(function(err){
-    if (err) throw err;
-    console.log("Connected!");
-    let herosTable = "CREATE TABLE IF NOT EXISTS `heroes` (`idx` int NOT NULL AUTO_INCREMENT, `name` varchar(50) NOT NULL, `alias` varchar(50) NOT NULL, `email` varchar(50) NOT NULL, `skills` varchar(50) NOT NULL, PRIMARY KEY (`idx`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
-    let skillsTable = "CREATE TABLE IF NOT EXISTS `skills` (`skill_index` int NOT NULL AUTO_INCREMENT, `skill_name` varchar(50) UNIQUE NOT NULL, PRIMARY KEY (`skill_index`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
-    let joinedTable = "CREATE TABLE IF NOT EXISTS `has_skills` (`hereos_idx` int NOT NULL, `skill_idx` int NOT NULL, PRIMARY KEY (`hereos_idx`,`skill_idx`), KEY `skill_idx` (`skill_idx`), CONSTRAINT `has_skills_ibfk_1` FOREIGN KEY (`hereos_idx`) REFERENCES `heroes` (`idx`), CONSTRAINT `has_skills_ibfk_2` FOREIGN KEY (`skill_idx`) REFERENCES `skills` (`skill_index`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
-    database.query(herosTable,function(err, result){
-        if(err) throw err;
-        console.log("Table Heroes Created!");
-    })
-    database.query(skillsTable,function(err, result){
-        if(err) throw err;
-        console.log("Table Skills Created!");
-    })
-    database.query(joinedTable,function(err, result){
-        if(err) throw err;
-        console.log("Table HeroSkills Created!");
-    })
-    
+  if (err) throw err;
+  console.log("Connected!");
+  let heroesTable = "CREATE TABLE IF NOT EXISTS `heroes` (`idx` int NOT NULL AUTO_INCREMENT, `name` varchar(50) NOT NULL, `alias` varchar(50) NOT NULL, `email` varchar(50) NOT NULL, PRIMARY KEY (`idx`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+  let skillsTable = "CREATE TABLE IF NOT EXISTS `skills` (`skill_index` int NOT NULL AUTO_INCREMENT, `skill_name` varchar(50) UNIQUE NOT NULL, PRIMARY KEY (`skill_index`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+  let joinedTable = "CREATE TABLE IF NOT EXISTS `has_skills` (`hereos_idx` int NOT NULL, `skill_idx` int NOT NULL, PRIMARY KEY (`hereos_idx`,`skill_idx`), KEY `skill_idx` (`skill_idx`), CONSTRAINT `has_skills_ibfk_1` FOREIGN KEY (`hereos_idx`) REFERENCES `heroes` (`idx`), CONSTRAINT `has_skills_ibfk_2` FOREIGN KEY (`skill_idx`) REFERENCES `skills` (`skill_index`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+  database.query(heroesTable,function(err, result){
+      if(err) throw err;
+      console.log("Table Heroes Created!");
+  })
+  database.query(skillsTable,function(err, result){
+      if(err) throw err;
+      console.log("Table Skills Created!");
+  })
+  database.query(joinedTable,function(err, result){
+      if(err) throw err;
+      console.log("Table HeroSkills Created!");
+  })
+  
 })
+
 
 const win = new QMainWindow();
 win.setWindowTitle("Hello World Test");
@@ -37,25 +39,49 @@ centralWidget.setObjectName("myroot");
 const rootLayout = new FlexLayout();
 centralWidget.setLayout(rootLayout);
 
-const label = new QLabel();
-label.setObjectName("topLabel");
-label.setText("Welcome to the list of Super Heros");
+const welcomeLbl = new QLabel();
+welcomeLbl.setObjectName("welcomeLbl");
+welcomeLbl.setText("Welcome to the list of Super Heroes");
 
+
+// Adds hero to the database with contents of 3 QLineEdit fields
 const addSuperHerobutton = new QPushButton();
 addSuperHerobutton.setText("Add Super Hero");
-const heroName = new QPlainTextEdit();
+
+const heroNameLbl = new QLabel();
+heroNameLbl.setObjectName("heroNameLbl");
+heroNameLbl.setText("Enter Hero Name:");
+const heroName = new QLineEdit();
 heroName.setObjectName("heroName");
-const heroAlias = new QPlainTextEdit();
-heroAlias.setObjectName("heroName");
-const heroEmail = new QPlainTextEdit();
-heroEmail.setObjectName("heroName");
+
+const heroAliasLbl = new QLabel();
+heroAliasLbl.setObjectName("heroAliasLbl");
+heroAliasLbl.setText("Enter Hero Alias:");
+const heroAlias = new QLineEdit();
+heroAlias.setObjectName("heroAlias");
+
+const heroEmailLbl = new QLabel();
+heroEmailLbl.setObjectName("heroEmailLbl");
+heroEmailLbl.setText("Enter Hero Email:");
+const heroEmail = new QLineEdit();
+heroEmail.setObjectName("heroEmail");
 addSuperHerobutton.addEventListener('clicked', ()=>{
-  let theHeroName = heroName.toPlainText();
-  let theHeroAlias = heroAlias.toPlainText();
-  let theHeroEmail = heroEmail.toPlainText();
-  let addHero = "INSERT INTO `heroes`(`name`,`alias`,`email`) values (" + theHeroName + "," + theHeroAlias + "," + theHeroEmail + ");"
-  console.log(addHero);
+
+    let theHeroName = heroName.text();
+    let theHeroAlias = heroAlias.text();
+    let theHeroEmail = heroEmail.text();
+    //let addHero = "INSERT INTO `heroes`(`name`,`alias`,`email`) values (" + theHeroName + "," + theHeroAlias + "," + theHeroEmail + ");"
+    let addHero = "INSERT INTO `heroes`(`name`,`alias`,`email`) values ('" + theHeroName + "','" + theHeroAlias + "','" + theHeroEmail + "');"
+    console.log(addHero);
+
+    database.query(addHero,function(err, result){
+       if(err) throw err;
+       console.log("Hero Added!");
+   })
+    
 })
+  
+
 
 const menu = new QMenu();
 
@@ -85,11 +111,16 @@ const removeSuperHeros = new QPushButton();
 removeSuperHeros.setText("Add Super Hero");
 
 
-rootLayout.addWidget(label);
+rootLayout.addWidget(welcomeLbl);
+
 rootLayout.addWidget(addSuperHerobutton);
+rootLayout.addWidget(heroNameLbl);
 rootLayout.addWidget(heroName);
+rootLayout.addWidget(heroAliasLbl);
 rootLayout.addWidget(heroAlias);
+rootLayout.addWidget(heroEmailLbl);
 rootLayout.addWidget(heroEmail);
+
 rootLayout.addWidget(listSuperHeros);
 rootLayout.addWidget(showSuperHeros);
 rootLayout.addWidget(removeSuperHeros);
@@ -114,3 +145,4 @@ win.setStyleSheet(
   `
 );
 win.show();
+global.win = win; // To prevent win from being garbage collected.
